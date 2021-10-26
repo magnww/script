6388eebf0b561b82c0c03934a636068daa61c657
magnww <vilosantos@gmail.com>
2021-10-26 20:24:24 +0800

    add enable TCP bbr

--- deploy.sh
+++ deploy.sh
@@ -61,6 +61,13 @@ iptables -P OUTPUT ACCEPT
 
 service netfilter-persistent save
 
+# enable tcp bbr
+sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
+sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
+echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf 
+echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+sysctl -p
+
 # install docker
 apt remove -y docker docker-engine docker.io containerd runc
 
